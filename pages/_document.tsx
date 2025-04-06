a20d3b02e348fed084bb326322497b14c142b8cb
NON-TITTLE <189063880+NON-TITTLE@users.noreply.github.com>
2025-04-06 01:59:21 -0600

    feat: add Mastodon and Nostr verification links

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="me" href="https://mastodon.social/@amazing_pizzadev" />
+        <link rel="me" href="https://me.pizza.blog" />
       </Head>
       <body>
         <Main />
